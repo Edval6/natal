72b0aad843213d2fed19968c04f45eeb81ceba36
Edival Tembe <edvaltembe@gmail.com>
2022-12-24 01:05:20 +0200

    pode escrever mensagem personalizada e o nome

--- js/script.js
+++ js/script.js
@@ -2,12 +2,20 @@
 var btn = document.querySelector("#btn");
 
 btn.onclick = function(){
-    var input = document.querySelector("input");
+
+    // Mensagem Personalizada
+    var input = document.querySelector("#mensagem");
     var msg = input.value;
 
     var h1 = document.querySelector("h1");
     h1.textContent = msg;
 
+
+    //Nome 
+
+    var input2 = document.querySelector("#nom");
+    var nome = input2.value;
+
     var h3 = document.querySelector("h3");
-    h3.textContent = "";
+    h3.textContent = nome;
 }
